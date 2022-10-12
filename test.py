182bc357db9f21e295933d3d820c594e3b178c82
Roger Esteve <rogeresteve30@gmail.com>
2022-10-12 17:38:54 +0200

    processed elements data

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import untis_tcm_wrapper
 
 data = untis_tcm_wrapper.get(1, 3182, "2022-10-11", 2)
 
-print(data)
+rooms, teachers, lessons = untis_tcm_wrapper.parse_elements(data["elements"])
--- untis_tcm_wrapper.py
+++ untis_tcm_wrapper.py
@@ -5,20 +5,31 @@ def get(element_type, element_id, date, format_id=2):
     headers = {'Cookie': 'traceId=66c8f4436e17811a4ac3bd33f9d9f85c3b924dd2; schoolname="_ZnVuZGFjacOzIHRlY25vY2FtcHVz"; schoolname="_ZnVuZGFjacOzIHRlY25vY2FtcHVz"; traceId=66c8f4436e17811a4ac3bd33f9d9f85c3b924dd2; JSESSIONID=3BA4296BE0CDFDAB57FD3E9E0CC32C2C'}
 
     url = "https://mese.webuntis.com/WebUntis/api/public/timetable/weekly/data?elementType=" + str(element_type) + "&elementId=" + str(element_id) + "&date=" + date + "&formatId=" + str(format_id)
-    print(url)
+
     req = requests.get(url, headers=headers)
 
     return json.loads(req.text)["data"]["result"]["data"]
 
 def parse_elements(elements):
-    print(elements)
     room_numbers = {}
     teacher_names = {}
     lesson_names = {}
 
+    for el in elements:
+        if el["type"] == 2:
+            teacher_names[str(el["id"])] = {
+                "name": el["longName"]
+            }
+        elif el["type"] == 3:
+            lesson_names[str(el["id"])] = {
+                "name": el["longName"]
+            }
+        elif el["type"] == 4:
+            room_numbers[str(el["id"])] = {
+                "name": el["name"]
+            }
 
-
-    room_numbers, teacher_names, lesson_names
+    return room_numbers, teacher_names, lesson_names
 
 class Lesson:
     def __init__(self):
